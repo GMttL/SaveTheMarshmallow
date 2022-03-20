1e669ebaa546e460c0e35cd6084a838343c1bc01
GMttL <gaby.mititelu@gmail.com>
2022-03-20 21:06:25 +0000

    1. Added block/unblock functionality. All works well, class seems to be more or less done.
    TODO:
    1. Create class to clear the operating system's cache
    2. Create class to manage the module's execution. Like a facade almost. (Keep track of blocked websites, sanitise input that gets passed in, etc.)
    3. Create launcher-class that asks the user for root-privileges.
    4. Start "live" testing of module.

--- com.gabrielmttl.websiteBlocker/src/WebsiteBlocker.java
+++ com.gabrielmttl.websiteBlocker/src/WebsiteBlocker.java
@@ -1,18 +1,24 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public final class WebsiteBlocker {
 
     private static final WebsiteBlocker instance = new WebsiteBlocker();
 
     private static String OS;
-    private static String hostFile;
+    private String hostFile;
 
 
     private WebsiteBlocker() {
+
         OS = System.getProperty("os.name").toLowerCase();
         if (OS.contains("win")) {
-            hostFile = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+            hostFile = "C:\\Windows\\System32\\drivers\\etc\\hostsTestFile";
         }
         else if (OS.contains("mac") || OS.contains("nux")) {
-            hostFile = "/etc/hosts";
+            hostFile = "/etc/hostsTestFile"; // TODO: change this line to point to /etc/hosts
         }
         else {
             // unknown OS. We cannot perform action.
@@ -38,5 +44,43 @@ public final class WebsiteBlocker {
         return null;
     }
 
+    public boolean blockWebsite(String url) {
+        // TODO: clear system dns cache as well to successfully block the website. Different responsibility. Need new class.
+        try {
+            Files.write(Paths.get(hostFile), ("\n0.0.0.0 " + url).getBytes(), StandardOpenOption.APPEND);
+            return true;
+        } catch (IOException e) {
+            System.err.println("WebsiteBlocker.blockWebsite -- Could not write to host file.");
+        }
+        return false;
+    }
 
+    public boolean unblockWebsite(String url) {
+        File inputFile = new File(hostFile);
+        File tempFile = new File(hostFile + ".tmp");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(hostFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+            String currentLine;
+
+            while ((currentLine = reader.readLine()) != null) {
+                if (currentLine.contains(url) && currentLine.contains("0.0.0.0") || currentLine.isEmpty()) continue;
+                writer.write(currentLine + System.getProperty("line.separator"));
+            }
+
+            writer.close();
+            reader.close();
+
+            return tempFile.renameTo(inputFile);
+        } catch (FileNotFoundException e) {
+            System.err.println("WebsiteBlocker.unblockWebsite -> could not find the file to open for reading. " +
+                    "hostFile location corrupt or non-existent.");
+            return false;
+        } catch (IOException e) {
+            System.err.println("WebsiteBlocker.unblockWebsite -> could not open file for writing.");
+            return false;
+        }
+
+    }
 }
--- com.gabrielmttl.websiteBlocker/tests/WebsiteBlockerTests.java
+++ com.gabrielmttl.websiteBlocker/tests/WebsiteBlockerTests.java
@@ -1,5 +1,6 @@
 import org.testng.annotations.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class WebsiteBlockerTests {
@@ -10,4 +11,15 @@ public class WebsiteBlockerTests {
         assertNotNull(wblocker, "websiteBlocker creation failed on Mac OS");
     }
 
+    @Test
+    public void testSuccessfulBlockingOfWebsite() {
+        assertNotNull(WebsiteBlocker.getInstance(), "WebsiteBlocker is null. Unrecognised operating system.");
+        assertTrue(WebsiteBlocker.getInstance().blockWebsite("facebook.com"), "Failed to block website.");
+    }
+
+    @Test
+    public void testSuccessfullUnblockingWebsite() {
+        assertNotNull(WebsiteBlocker.getInstance(), "WebsiteBlocker is null. Unrecognised operating system.");
+        assertTrue(WebsiteBlocker.getInstance().unblockWebsite("facebook.com"), "Failed to unblock website.");
+    }
 }
