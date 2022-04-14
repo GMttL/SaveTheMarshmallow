1662b2a4a7092b563fa8c97515087021a6826142
GMttL <gaby.mititelu@gmail.com>
2022-04-14 16:14:58 +0300

    Abstracted away scene creation

---
+++ UI/src/main/java/com/gabrielmttl/savethemarshmallow/SaveTheMarshmallowModel.java
@@ -0,0 +1,5 @@
+package com.gabrielmttl.savethemarshmallow;
+
+public class SaveTheMarshmallowModel {
+
+}
--- UI/src/main/java/com/gabrielmttl/savethemarshmallow/ui/savethemarshmallowui/SaveTheMarshmallowApplication.java
+++ UI/src/main/java/com/gabrielmttl/savethemarshmallow/ui/savethemarshmallowui/SaveTheMarshmallowApplication.java
@@ -3,14 +3,22 @@ package com.gabrielmttl.savethemarshmallow.ui.savethemarshmallowui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SaveTheMarshmallowApplication extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
+        presentMainStage(stage);
+    }
+
+    private void presentMainStage(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SaveTheMarshmallowApplication.class.getResource("MainStage-View.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 420);
 
@@ -26,9 +34,5 @@ public class SaveTheMarshmallowApplication extends Application {
         // Set and Show Scene
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
